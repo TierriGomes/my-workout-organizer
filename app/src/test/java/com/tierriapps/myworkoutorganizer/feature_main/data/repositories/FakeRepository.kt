03c2a2612b2df66a0bae1210b783e5575ebe2751
Tierri Gomes <tierrigomes@gmail.com>
2023-07-27 10:42:04 -0300

    created a fakeRepository for unit tests

---
+++ app/src/test/java/com/tierriapps/myworkoutorganizer/feature_main/data/repositories/FakeRepository.kt
@@ -0,0 +1,84 @@
+package com.tierriapps.myworkoutorganizer.feature_main.data.repositories
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.tierriapps.myworkoutorganizer.R
+import com.tierriapps.myworkoutorganizer.common.utils.Resource
+import com.tierriapps.myworkoutorganizer.common.utils.UiText
+import com.tierriapps.myworkoutorganizer.feature_main.domain.models.Workout
+import com.tierriapps.myworkoutorganizer.feature_main.domain.repositories.WorkoutRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class FakeRepository: WorkoutRepository {
+    var shouldReturnSuccess = true
+    var listOfWorkouts = listOf(
+        Workout(1, "name1", "description"),
+        Workout(2, "name2", "description")
+    )
+    override fun getAllWorkouts(): Flow<Resource<List<Workout>>?> = flow {
+        emit(Resource.Loading())
+        if (shouldReturnSuccess){
+            emit(Resource.Success(listOfWorkouts, UiText.StringResource(R.string.success)))
+        }
+        else {
+            emit(Resource.Error(null, UiText.StringResource(R.string.error)))
+        }
+    }
+
+    override fun getWorkoutById(id: Int): Flow<Resource<Workout?>> = flow {
+        emit(Resource.Loading())
+        if (shouldReturnSuccess){
+            emit(Resource.Success(listOfWorkouts[1], UiText.StringResource(R.string.success)))
+        }
+        else {
+            emit(Resource.Error(null, UiText.StringResource(R.string.error)))
+        }
+    }
+
+    override fun insertWorkout(workout: Workout): Flow<Resource<Workout?>> = flow {
+        emit(Resource.Loading())
+        if (shouldReturnSuccess){
+            emit(Resource.Success(listOfWorkouts[1], UiText.StringResource(R.string.success)))
+        }
+        else {
+            emit(Resource.Error(null, UiText.StringResource(R.string.error)))
+        }
+    }
+
+    override fun deleteWorkout(workout: Workout): Flow<Resource<Workout?>>  = flow {
+        emit(Resource.Loading())
+        if (shouldReturnSuccess){
+            emit(Resource.Success(listOfWorkouts[1], UiText.StringResource(R.string.success)))
+        }
+        else {
+            emit(Resource.Error(null, UiText.StringResource(R.string.error)))
+        }
+    }
+
+    override suspend fun getActualWorkoutId(): Resource<Int?> {
+        if (shouldReturnSuccess){
+            return Resource.Success(1, UiText.StringResource(R.string.success))
+        }
+        else {
+            return Resource.Error(null, UiText.StringResource(R.string.error))
+        }
+    }
+
+    override suspend fun setActualWorkoutId(id: Int): Resource<Int?> {
+        if (shouldReturnSuccess){
+            return Resource.Success(1, UiText.StringResource(R.string.success))
+        }
+        else {
+            return Resource.Error(null, UiText.StringResource(R.string.error))
+        }
+    }
+
+    override fun getUserId(): LiveData<String> {
+        return MutableLiveData("userId")
+    }
+
+    override fun getUserName(): LiveData<String> {
+        return MutableLiveData("userName")
+    }
+}
